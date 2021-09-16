9cc5239f5b7fa15d605bbd12bb24bd63a4308a7e
UlissesSilva08 <ulissesmasterchief@gmail.com>
2021-09-16 19:58:31 -0300

    Inserindo o comando string e convertendo para Int32

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo de medidas");
+
+            Console.WriteLine();
+
+            Console.Write("Digite a medida desejada(em metros): ");
+            string valorDigitado = Console.ReadLine();
+            int metros = Int32.Parse(valorDigitado);
+
+            Console.WriteLine();
+
+            Console.WriteLine("--- Equivalência ---");
+            Console.WriteLine("Medida em centimetros: " + metros*100 + " cm");
+            Console.WriteLine("Medida em metros: " + metros + " m");
+            Console.WriteLine("Medida em quilometros: " + metros*0.001 + " km");
+            
+        }
+    }
+}
